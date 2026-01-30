1fe3c1466946e867964d815f84f1b38da6a02ddd
ARPIT SINGHAL <161847701+ARPIT-SINGHAL-22102003@users.noreply.github.com>
2026-01-30 13:13:57 +0000

    first 2 problem soled

---
+++ PS-Sprint-1-Solutions/One.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class One{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter the number: ");
+        int n = s.nextInt();
+
+        if(n%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
